6d0f8388144c51a83428aa48ff70092f651a53e7
FariaMobeen <faria.mobeen@ucalgary.ca>
2023-02-28 21:42:37 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,69 @@
+
+import './App.css';
+import React, {useState} from "react";
+import ReactQuill from "react-quill";
+import "../node_modules/react-quill/dist/quill.snow.css";
+
 function App() {
-  return <h1>Lotion</h1>;
+  const [noteTitle, setNoteTitle] = useState("Untitled");
+
+  const [body,setBody] = useState("");
+  const handleBody = e =>{
+    console.log(e);
+    setBody(e);
+  }
+  return (
+    <div>
+      <header>
+        <h1>Lotion</h1>
+     <p>Like Notion, but worse</p>
+      </header>
+      <div className="App">
+        <div className="editor-container">
+        <div className="editor-header">
+        <input
+  type="text"
+  placeholder="Enter note title"
+  value={noteTitle}
+  onChange={(e) => setNoteTitle(e.target.value)}
+  className="note-title-input"
+/>
+
+
+</div>
+          <ReactQuill
+            placeholder="Your note here"
+            modules={App.modules}
+            formats={App.formats}
+            onChange={handleBody}
+            value={body}
+            className="my-quill-editor"
+          />
+        </div>
+        <div className="sidebar">
+          <p><strong>Notes</strong></p>
+        </div>
+      </div>
+    </div>
+  );
 }
+App.modules = {
+  toolbar: [
+    [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, 
+     {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image', 'video'],
+    ['clean']
+  ],
+};
+
+App.formats = [
+  'header', 'font', 'size',
+  'bold', 'italic', 'underline', 'strike', 'blockquote',
+  'list', 'bullet', 'indent',
+  'link', 'image', 'video'
+];
 
 export default App;
